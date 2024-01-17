e18076ee2c84f784382be644bca0a5e493415532
123qwertyuiop123 <1443819827@qq.com>
2024-01-17 11:05:03 +0800

    导航栏

---
+++ components/MtHeader.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="MtHeader">
+    <img src="../assets/image/MtHeader/logo.webp" alt="" srcset="">
+    <div v-for="(item, index) in menuList" :key="index" class="menu-item">
+      {{ item.name }}
+    </div>
+    <div>{{ phone }}</div>
+    <div class="btn">免费使用</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MtHeader',
+  data() {
+    return {
+
+      menuList: [
+        {
+          name: '产品技术',
+        },
+        {
+          name: '解决方案',
+        },
+        {
+          name: '客户案例',
+        },
+        {
+          name: '生态体系',
+        },
+        {
+          name: '咨询动态',
+        },
+        {
+          name: '关于明途',
+        },
+      ],
+      phone: '400-001-0224/028-8562-52244'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.MtHeader {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px 0;
+}
+.menu-item{
+  padding: 0 25px;
+}
+.btn{
+
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,39 @@
 <template>
-  <div>IndexPage</div>
+  <div class="app">
+    <MtHeader class="mt-header"></MtHeader>
+  </div>
 </template>
 
 <script>
+import MtHeader from '../components/MtHeader.vue';
 export default {
   name: 'IndexPage',
+  components: {
+    MtHeader
+  },
+  data() {
+    return {
+      a: 1
+    }
+  },
+  //独立设置head信息
+  head() {
+    return {
+      title: '成都明途科技有限公司-中国专业的目标大数据厂商',
+      meta: [
+        { name: 'description', content: '成都明途科技有限公司（MINTO）成立于2014年，总部设立于成都，专注于目标管理大数据领域，以互联网、大数据和人工智能技术为支撑，精心打造督查督办、目标绩效、重点项目等管理系统。' }
+      ]
+    }
+  }
 }
 </script>
+<style scoped>
+.app {
+  width: 100%;
+}
+
+.mt-header {
+  position: fixed;
+  top: 0;
+}
+</style>
